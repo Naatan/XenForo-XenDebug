b4e055ac5ee3e8fb1257fb1a58170b0924ea5882
Nathan Rijksen <n.rijksen@gmail.com>
2012-05-11 11:13:25 -0400

    Check if class already exists before initialization to prevent crashes where Error class was initialized earlier than expected

--- library/XenDebug/XenForo/Error.php
+++ library/XenDebug/XenForo/Error.php
@@ -1,36 +1,41 @@
 <?php
 
-abstract class XenForo_Error extends XenForo_Error_Abstract
+if ( ! class_exists('XenForo_Error'))
 {
-
-	public static function unexpectedException(Exception $e)
+	
+	abstract class XenForo_Error extends XenForo_Error_Abstract
 	{
-		if ($log = XenDebug_Log::getInstance())
+	
+		public static function unexpectedException(Exception $e)
 		{
-			$log->logException($e);
+			if ($log = XenDebug_Log::getInstance())
+			{
+				$log->logException($e);
+			}
+	
+			return parent::unexpectedException($e);
 		}
-
-		return parent::unexpectedException($e);
-	}
-
-	public static function logException(Exception $e, $rollbackTransactions = true)
-	{
-		if ($log = XenDebug_Log::getInstance())
+	
+		public static function logException(Exception $e, $rollbackTransactions = true)
 		{
-			$log->logException($e);
+			if ($log = XenDebug_Log::getInstance())
+			{
+				$log->logException($e);
+			}
+	
+			return parent::logException($e, $rollbackTransactions);
 		}
-
-		return parent::logException($e, $rollbackTransactions);
-	}
-
-	public static function debug($message)
-	{
-		if ($log = XenDebug_Log::getInstance())
+	
+		public static function debug($message)
 		{
-			$log->logDebugMessage(call_user_func_array('sprintf', func_get_args()));
+			if ($log = XenDebug_Log::getInstance())
+			{
+				$log->logDebugMessage(call_user_func_array('sprintf', func_get_args()));
+			}
+	
+			call_user_func_array(array('parent','debug'), func_get_args());
 		}
-
-		call_user_func_array(array('parent','debug'), func_get_args());
+	
 	}
 
 }
